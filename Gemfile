39c180879ac47646140da49b23285bb7e1bdd0a8
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2013-11-12 00:06:00 +0000

    initial commit with basic setup and cleaned up files

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'mysql2'
+
+gem 'jbuilder'
+
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+# gem 'jbuilder'
+
+#insert into users (name,username,password,email,created_at,updated_at) VALUES ('alex','alexstoick','needrealpassword','cba@yahoo.com',now(),now());
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EssayBackend::Application.config.secret_token = 'eb46b094d2b6a1f071c6655bc691ee36db46fd7ee8f213747d36b0784f2e266c6264cad4ea60e6c981f44dc0d97c84bcd9250301887fde786e117c7a907b45ab'
