f80a4ebd660dd58107384c59562dbd242e043760
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2013-11-16 22:52:55 +0000

    implemented update&delete for replies

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -6,6 +6,10 @@ class ReplyController < ApplicationController
 		content = params[:description]
 		parent_id = params[:parent_id]
 
+		if ( title.nil? || content.nil? || parent_id.nil? )
+			render json: { "error" => "Wrong parameters" }, :status => :bad_request
+		end
+
 		reply = Reply.new
 		reply.title = title
 		reply.content = content
@@ -17,8 +21,30 @@ class ReplyController < ApplicationController
 	end
 
 	def update
+
+		title = params[:title]
+		content = params[:description]
+		parent_id = params[:parent_id]
+
+		reply = Reply.find(params[:reply_id])
+		if ( ! title.nil? )
+			reply.title = title
+		end
+		if ( ! content.nil? )
+			reply.content = content
+		end
+		if ( ! parent_id.nil? )
+			reply.parent_id = parent_id
+		end
+		render json: { "success" => reply.save , "id" => reply.id }
+
 	end
 
 	def delete
+
+		id = params[:reply_id]
+
+		render json: { "success" => ( Reply.delete(id) == 1 )}
+
 	end
 end
