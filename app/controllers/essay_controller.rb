eb8cf9629c68cb95a7d6798726cefa40c4e85977
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2013-11-17 13:32:50 +0000

    tags are now working.

--- app/controllers/essay_controller.rb
+++ app/controllers/essay_controller.rb
@@ -13,6 +13,7 @@ class EssayController < ApplicationController
 
 		title = params[:title]
 		description = params[:description]
+		tag = params[:tag]
 
 		if ( title.nil? || description.nil? )
 			render json: { "error" => "Wrong parameters" }, :status => :bad_request
@@ -23,7 +24,29 @@ class EssayController < ApplicationController
 		essay.description = description
 		essay.user_id = 0
 
-		render json: { "success" => essay.save , "id" => essay.id }
+		if ( ! essay.save )
+			render json: { "error" => "Cannot save to database"}, :status => :internal_server_error
+			return
+		end
+
+
+		if ( ! tag.nil? )
+			tags = tag.split(",")
+			tags.each do |tagName|
+				tag = Tag.find_by_name( tagName )
+				if ( tag.nil? )
+					tag = Tag.new
+					tag.name = tagName
+					tag.save!
+				end
+				essay_tag = EssayTag.new
+				essay_tag.essay_id = essay.id
+				essay_tag.tag_id = tag.id
+				essay_tag.save!
+			end
+		end
+
+		render json: { "success" =>  true , "id" => essay.id }
 
 	end
 
